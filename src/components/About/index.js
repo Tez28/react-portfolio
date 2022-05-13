9abff7c9793535494fae49bb85cba655687ec2fa
Tez28 <courtezcannady@gmail.com>
2022-05-13 18:21:32 -0400

    initial files

---
+++ src/components/About/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import profileImage from "../../assets/profile/profile.jpg"
+
+function About() {
+
+  return (
+    <section className="my-5" >
+      <h1 id="about">Full Stack Developer</h1>
+      <img src={profileImage} className="my-2" style={{ width: "10%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+            Hello my name is Courtez Cannady. I am a Full Stack Developer with Experience in 
+            systems engineering and design patterns. After serving in the U.S. Marine Corps I
+            went on to earn a bachelors degree in I.T. Management, AWS Certifications such as 
+            Certified Solutions Architect and Certified Cloud Practitioner and also hold the
+            ITIL v4 Foundation. Currently I work as a Sr. Systems Engineer primarily in the realm
+            of model based engineering. I am hoping to move into a Solutions Architect/Software 
+            engineering role. I am passionate about learning new technologies and applying them where
+            I can.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default About;
