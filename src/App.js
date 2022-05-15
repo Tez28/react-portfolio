6bf067980612f93aba55c42ab29088bfe4eabc40
Tez28 <courtezcannady@gmail.com>
2022-05-15 11:34:55 -0400

    add header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import About from './components/About'
+import About from './components/Header'
 
 function App() {
   return (
     <div className="App">
       <div>
-        <About></About>
+        <Header></Header>
       </div>
     </div>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+//import Navigation from "./Nav";
+//import About from "./About";
+//import Contact from "./Contact";
+//import Portfolio from "./Portfolio";
+//import Resume from "./Resume";
+
+function Header () {
+    const [currentPage, handlePageChange] = useState("About");
+    const renderPort = () => {
+        switch (currentPage) {
+            case "About":
+                return <About/>;
+            case "Portfolio":
+                return <Portfolio/>;
+            case "Contact":
+                return <Contact/>;
+            case "Resume":
+                return <Resume/>;
+            default:
+                return <About/>;    
+        }
+    };
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="navbar-brand">
+                    <a className="navbar-git" rel="noreferrer" target="_blank" href="https://github.com/Tez28" >
+                        <span className="content is-large" > Courtez M. Cannady</span>
+                    </a>
+                </div>
+            </nav>
+            <Navi
+                currentPage={currentPage}
+                handlePageChange={handlePageChange}
+            />
+            <main>
+                <div>{renderPort(currentPage)}</div>
+            </main>
+        </div>
+    );
+}
+export default Header;
